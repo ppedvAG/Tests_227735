11656fa58788795698fcc11c3ec533c336bea321
André <AndreR@ppedv.de>
2023-06-12 14:06:40 +0200

    IsWeekend

---
+++ TDDBank/TDDBank.Tests/OpeningHoursTest.cs
@@ -0,0 +1,38 @@
+﻿namespace TDDBank.Tests
+{
+    public class OpeningHoursTests
+    {
+        [Theory]
+        [InlineData(2023, 1, 09, 10, 30, true)]//mo
+        [InlineData(2023, 1, 09, 10, 29, false)]//mo
+        [InlineData(2023, 1, 09, 10, 31, true)] //mo
+        [InlineData(2023, 1, 09, 18, 59, true)] //mo
+        [InlineData(2023, 1, 09, 19, 00, false)] //mo
+        [InlineData(2023, 1, 14, 13, 0, true)] //sa
+        [InlineData(2023, 1, 14, 16, 0, false)] //sa
+        [InlineData(2023, 1, 15, 20, 0, false)] //so
+        public void OpeningHours_IsOpen(int y, int M, int d, int h, int m, bool result)
+        {
+            var dt = new DateTime(y, M, d, h, m, 0);
+            var oh = new OpeningHours();
+
+
+            Assert.Equal(result, oh.IsOpen(dt));
+        }
+
+        [Fact]
+        public void IsWeekend()
+        {
+            var oh = new OpeningHours();
+
+            Assert.False(oh.IsWeekend());
+            Assert.False(oh.IsWeekend());
+            Assert.False(oh.IsWeekend());
+            Assert.False(oh.IsWeekend());
+            Assert.False(oh.IsWeekend());
+            Assert.True(oh.IsWeekend());
+            Assert.True(oh.IsWeekend());
+        }
+
+    }
+}
---
+++ TDDBank/TDDBank/OpeningHours.cs
@@ -0,0 +1,28 @@
+﻿namespace TDDBank
+{
+    public class OpeningHours
+    {
+        public bool IsOpen(DateTime dt)
+        {
+            var start = new TimeSpan(10, 30, 0);
+            var ende = new TimeSpan(19, 00, 0);
+            var endeSa = new TimeSpan(14, 00, 0);
+
+            //häßlich aber geht
+            if (dt.DayOfWeek == DayOfWeek.Sunday) return false;
+            else if (dt.DayOfWeek == DayOfWeek.Saturday && dt.TimeOfDay >= start && dt.TimeOfDay < endeSa)
+                return true;
+            else if (dt.DayOfWeek != DayOfWeek.Saturday && dt.DayOfWeek != DayOfWeek.Sunday && dt.TimeOfDay >= start && dt.TimeOfDay < ende)
+                return true;
+
+            return false;
+        }
+
+        public bool IsWeekend()
+        {
+            return DateTime.Now.DayOfWeek == DayOfWeek.Sunday ||
+                   DateTime.Now.DayOfWeek == DayOfWeek.Saturday;
+        }
+
+    }
+}
