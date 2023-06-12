95f6fb9a3082bc0d9202aa1cbb6c497a1e0d200b
André <AndreR@ppedv.de>
2023-06-12 10:57:19 +0200

    OverFlowTest und InLineData

--- Calculator/Calculator.Tests/CalcTests.cs
+++ Calculator/Calculator.Tests/CalcTests.cs
@@ -14,5 +14,44 @@ namespace Calculator.Tests
             //Assert
             Assert.Equal(7, result);
         }
+
+        [Fact]
+        public void Sum_0_and_0_results_0()
+        {
+            //Arrange
+            Calc calc = new Calc();
+
+            //Act
+            var result = calc.Sum(0, 0);
+
+            //Assert
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Sum_MAX_and_1_throws_OverflowException()
+        {
+            Calc calc = new Calc();
+
+            Assert.Throws<OverflowException>(() => calc.Sum(int.MaxValue, 1));
+        }
+
+        [Theory]
+        [InlineData(0, 0, 0)]
+        [InlineData(5, 4, 9)]
+        [InlineData(-5, -4, -9)]
+        [InlineData(-5, 7, 2)]
+        public void Sum_good_values(int a, int b, int exp)
+        {
+            //Arrange
+            Calc calc = new Calc();
+
+            //Act
+            var result = calc.Sum(a, b);
+
+            //Assert
+            Assert.Equal(exp, result);
+        }
+
     }
 }
