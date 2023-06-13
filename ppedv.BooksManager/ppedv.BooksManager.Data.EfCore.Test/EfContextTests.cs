97e5c284eb6d1b03f85356211c251401e0565d6d
André <AndreR@ppedv.de>
2023-06-13 11:07:52 +0200

    CRUD Test für EfCore

--- ppedv.BooksManager/ppedv.BooksManager.Data.EfCore.Test/EfContextTests.cs
+++ ppedv.BooksManager/ppedv.BooksManager.Data.EfCore.Test/EfContextTests.cs
@@ -1,3 +1,5 @@
+using ppedv.BooksManager.Model;
+
 namespace ppedv.BooksManager.Data.EfCore.Test
 {
     public class EfContextTests
@@ -5,6 +7,7 @@ namespace ppedv.BooksManager.Data.EfCore.Test
         string conString = "Server=(localdb)\\mssqllocaldb;Database=BooksManager_Dev;Trusted_Connection=true";
 
         [Fact]
+        [Trait("", "Integration")]
         public void Can_create_DB()
         {
             var con = new EfContext(conString);
@@ -13,6 +16,85 @@ namespace ppedv.BooksManager.Data.EfCore.Test
             var result = con.Database.EnsureCreated();
 
             Assert.True(result);
+        }
+
+        [Fact]
+        [Trait("", "Integration")]
+        public void Can_create_Book()
+        {
+            var book = new Book() { Title = "Testbook" };
+            var con = new EfContext(conString);
+            con.Add(book);
+            var result = con.SaveChanges();
+
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        [Trait("", "Integration")]
+        public void Can_read_Book()
+        {
+            var book = new Book() { Title = $"Testbook_{Guid.NewGuid()}" };
+            using (var con = new EfContext(conString))
+            {
+                con.Add(book);
+                con.SaveChanges();
+            }
+
+            using (var con = new EfContext(conString))
+            {
+                var loaded = con.Books.Find(book.Id);
+                Assert.Equal(book.Title, loaded.Title);
+            }
+        }
+
+        [Fact]
+        [Trait("", "Integration")]
+        public void Can_update_Book()
+        {
+            var book = new Book() { Title = $"Testbook_{Guid.NewGuid()}" };
+            var newTitle = $"NewBookTitle_{Guid.NewGuid()}";
+            using (var con = new EfContext(conString))
+            {
+                con.Add(book);
+                con.SaveChanges();
+            }
+            using (var con = new EfContext(conString))
+            {
+                var loaded = con.Books.Find(book.Id);
+                loaded.Title = newTitle;
+                var rows = con.SaveChanges();
+                Assert.Equal(1, rows);
+            }
+            using (var con = new EfContext(conString))
+            {
+                var loaded = con.Books.Find(book.Id);
+                Assert.Equal(newTitle, loaded.Title);
+            }
+        }
+
+        [Fact]
+        [Trait("", "Integration")]
+        public void Can_delete_Book()
+        {
+            var book = new Book() { Title = $"Testbook_{Guid.NewGuid()}" };
+            using (var con = new EfContext(conString))
+            {
+                con.Add(book);
+                con.SaveChanges();
+            }
+            using (var con = new EfContext(conString))
+            {
+                var loaded = con.Books.Find(book.Id);
+                con.Books.Remove(loaded);
+                var rows = con.SaveChanges();
+                Assert.Equal(1, rows);
+            }
+            using (var con = new EfContext(conString))
+            {
+                var loaded = con.Books.Find(book.Id);
+                Assert.Null(loaded);
+            }
         }
     }
 }
