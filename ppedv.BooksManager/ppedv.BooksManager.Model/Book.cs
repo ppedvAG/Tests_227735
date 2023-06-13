7257ea5f09b8f1366fe9d1e4c4dd4e847998404e
André <AndreR@ppedv.de>
2023-06-13 09:50:18 +0200

    Model für BooksManager

---
+++ ppedv.BooksManager/ppedv.BooksManager.Model/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace ppedv.BooksManager.Model
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int Pages { get; set; }
+        public DateTime PublishDate { get; set; }
+
+        public ICollection<Author> Authors { get; set; } = new HashSet<Author>();
+    }
+}
