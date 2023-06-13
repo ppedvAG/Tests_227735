7eba6ccf0ed159656a8bb7108302749005a0234a
André <AndreR@ppedv.de>
2023-06-13 10:00:31 +0200

    EfCore für BooksManager

---
+++ ppedv.BooksManager/ppedv.BooksManager.Data.EfCore/EfContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ppedv.BooksManager.Model;
+
+namespace ppedv.BooksManager.Data.EfCore
+{
+    public class EfContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+
+        string conString;
+
+        public EfContext(string conString)
+        {
+            this.conString = conString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(conString).UseLazyLoadingProxies();
+        }
+
+    }
+}
